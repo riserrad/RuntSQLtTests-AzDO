b7cd934fe8b077ac1e0d58be24349d1ca61d9e1e
Ricardo Serradas <rmserradas@gmail.com>
2019-04-06 18:51:46 -0700

    Adding the ability to run selected tests/improving guidance (#10)
    * PowerShell scripts adjusted to support running specific tests
    
    * Tweaking the task metadata to include the fields on the task on AzDO
    
    * Fine tune for fixing the issues #6 and #9

--- buildAndReleaseTask/Invoke-tSQLtTests.ps1
+++ buildAndReleaseTask/Invoke-tSQLtTests.ps1
@@ -1,14 +1,27 @@
 param(
     # Database info parameters    
     [string]$connectionString,
+    [string]$testOrClassName = "",
     
     # Test Result parameters
     [string]$testResultsFileName,
     [string]$queryTimeout
 )
 
-Invoke-SqlCmd -ConnectionString $connectionString -QueryTimeout $queryTimeout -Query "EXEC tSQLt.RunAll"
-Write-Output "Tests ran successfully. Saving test results to $testResultsFileName"
+if ($testOrClassName -eq "") {
+    Invoke-SqlCmd -ConnectionString $connectionString `
+    -QueryTimeout $queryTimeout `
+    -Query "EXEC tSQLt.RunAll"
+
+    Write-Output "Tests ran successfully. Saving test results to $testResultsFileName"    
+}
+else {
+    Invoke-SqlCmd -ConnectionString $connectionString `
+    -QueryTimeout $queryTimeout `
+    -Query "EXEC tSQLt.Run '$testOrClassName'"
+    
+    Write-Output "Tests ran successfully. Saving test results to $testResultsFileName"
+}
 
 Invoke-SqlCmd -ConnectionString $connectionString -QueryTimeout $queryTimeout -InputFile ".\GetTestResults.sql" | Select-Object -ExpandProperty XML* | Out-File -FilePath $testResultsFileName
 Write-Output "Finished gathering test results and writing it to $testResultsFileName"
